b85a0f43babee1466f3864f8978178951affe68b
pcuti <100185427+pcuti@users.noreply.github.com>
2022-02-27 17:15:43 +0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "hello.h"
-
-std::string printGameOver(){
-	// TODO: Return the required string
-	return "";
+using namespace std;
+string printGameOver(){
+	return "GameOver";
 }
